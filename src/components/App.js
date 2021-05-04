cee281609b01e99bc1ccae3593d1d3019f906db7
coltenappleby <colten.appleby@gmail.com>
2021-05-04 11:17:34 -0400

    finished showing items

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,29 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
+const API = `http://localhost:6001/listings`;
+
+
 function App() {
+
+  let [listings, updateListings] = useState([])
+
+  useEffect(() => {
+    fetch(API)
+      .then(r => r.json())
+      .then(listings => {
+        const list = listings.map((listing) => {
+          return {...listing, deleted: false, favorite: false}
+        });
+        updateListings(list)
+      });
+  }, []);
+
   return (
     <div className="app">
       <Header />
-      <ListingsContainer />
+      <ListingsContainer listings = {listings} />
     </div>
   );
 }
--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,16 @@
 import React from "react";
+import ListingCard from "./ListingCard";
 // import ListingCard from "./ListingCard";
 
-function ListingsContainer() {
+function ListingsContainer({listings}) {
+
+  const showListings = listings.map( (listing) => (
+    < ListingCard description = {listing.description} image = {listing.image} location = {listing.location} />
+  ))
   return (
     <main>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+        {showListings}
       </ul>
     </main>
   );
